d5f9b1ed26f784d823d054383e76ba58c1cf33c7
shahriar2880 <shahriarreza436@gmail.com>
2024-12-12 02:07:03 +0600

    coffee shop first commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import { Geist, Geist_Mono, Jost } from "next/font/google";
+import "./globals.css";
+
+const jost = Jost({subsets: ["latin"]})
+
+export const metadata = {
+  title: "Coffee Shop",
+  description: "Coffee Shop",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={jost.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
