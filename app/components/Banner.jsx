ea8a7b367c338c240ddf98b35e02de191ed4a912
shahriar2880 <shahriarreza436@gmail.com>
2025-01-31 02:30:06 +0600

    create a banner components

---
+++ app/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+        <div className='container'>
+            <h1>Hello Banner</h1>
+        </div> 
+    </>
+  )
+}
+
+export default Banner
